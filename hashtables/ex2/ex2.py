eee4f3aa21028a374c8a46a088712a17ef7bdf29
Mike Harley <mharley12345@gmail.com>
2020-02-14 09:10:50 -0800

    ex2 test passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,8 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
                         hash_table_retrieve,
-                        hash_table_resize)
+                        )
 
 
 class Ticket:
@@ -19,5 +18,18 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # let store all tickets in hashtable
+    for each_ticket in tickets:
+        hash_table_insert(hashtable, each_ticket.source,
+                          each_ticket.destination)
 
-    pass
+    # we know starting location has source of NONE, so we can store the respective destination in the route
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    # Now loop through the rest and order them according their sources
+    # Skip one in length as we already added first destination
+    # Use previous desintation as lookup for next following destination
+    for index in range(1, length):
+        route[index] = hash_table_retrieve(hashtable, route[index-2])
+
+    return route
